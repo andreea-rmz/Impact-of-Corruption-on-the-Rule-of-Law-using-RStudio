3b4e839f0d71248e6531636f42f6c8e87fdf37da
Andrea Ramírez <81031870+andreea-rmz@users.noreply.github.com>
2025-11-29 13:04:58 -0600

    Refactor WJP2022.R to read data from GitHub
    Updated the script to read data from a GitHub URL instead of a local file. Added comments for clarity and organized the code structure.

--- WJP2022.R
+++ WJP2022.R
@@ -1,12 +1,33 @@
 library(readxl)
-WJP_2022 <- read_excel("~/Documents/WJP_2022.xlsx")
-m1 <- lm(score ~ factor1+factor2+factor3+factor4+factor5+factor6+factor7+factor8, data=WJP_2022)
-summary(m1)
-cor(WJP_2022)
+library(httr)
 library(ggplot2)
-ggplot(data = WJP_2022, mapping = aes(x =factor2, y = `score`)) + geom_point() + geom_smooth(method="lm")
-pt(6.936e+14, 131, lower.tail = FALSE)
-library(caTools)
-install.packages('car')
 library(car)
+
+# 1. Read database from GitHub
+url <- "https://raw.githubusercontent.com/andreea-rmz/Impact-of-Corruption-on-the-Rule-of-Law-using-RStudio/main/data/WJP_2022.xlsx"
+
+temp <- tempfile(fileext = ".xlsx")
+GET(url, write_disk(temp, overwrite = TRUE))
+
+
+WJP_2022 <- read_excel(temp)
+
+# MLR
+m1 <- lm(score ~ factor1 + factor2 + factor3 + factor4 +
+           factor5 + factor6 + factor7 + factor8,
+         data = WJP_2022)
+summary(m1)
+
+# Corr matrix
+cor(WJP_2022)
+
+# factor2 vs score
+ggplot(WJP_2022, aes(x = factor2, y = score)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+# Hypothesis test
+pt(6.936e+14, df = 131, lower.tail = FALSE)
+
+# 6. VIF para multicolinealidad
 vif(m1)
